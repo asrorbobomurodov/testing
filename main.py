4635867550ce2eaff0bb3238365a19b8636f3c26
asrorbobomurodov <mr.aceonejr777@gmail.com>
2023-11-15 09:33:06 +0500

    done

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+import json
+def main(file_path: str):
+    f = open(file_path, "r")
+    file = f.read()
+    data = json.loads(file)
+    numbers = data['numbers']
+    return sum(numbers)
 
+print(main("data.txt"))
